330255cd937496e687c3291e63224187ea84deb8
DanteLaviolette <dante.laviolette@gmail.com>
2023-03-12 12:37:53 -0400

    init shared module

---
+++ images/shared/persistence/db.go
@@ -0,0 +1,112 @@
+package persistence
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/joho/godotenv"
+)
+
+var client *mongo.Client = nil
+var ctx context.Context
+var ctxCancel context.CancelFunc
+
+/*
+Initialize the database connection.
+*/
+func InitializeDBConnection() {
+	// Only initialize client if not set (singleton pattern)
+	if client == nil {
+		// Load .env file if not prod
+		loadDevEnv()
+		// Create context
+		ctx, ctxCancel = context.WithTimeout(context.Background(), 10*time.Second)
+		// Connect to mongoDB
+		var err error
+		client, err = mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URL")))
+		// Fail if connection error occurred
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Verify connection was made via pinging db
+		err = client.Ping(ctx, nil)
+		// Fail if connection error occurred
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Connected to MongoDB")
+		// Handle cleanup on exit
+		cleanupDBConnectionOnExit()
+		// Setup indices
+		setupIndices()
+	}
+}
+
+/*
+Returns the user collection along with its context
+*/
+func GetUserCollection() (mongo.Collection, context.Context) {
+	return *(client.Database(os.Getenv("DB_NAME")).
+		Collection(os.Getenv("USER_COLLECTION_NAME"))), ctx
+}
+
+/*
+Runs a background function to cleanup the database connection on
+exit.
+*/
+func cleanupDBConnectionOnExit() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	// Run exit func in background waiting for exit signal
+	go func() {
+		<-sigs
+		// Disconnect client
+		client.Disconnect(ctx)
+		// Cleanup context
+		ctxCancel()
+		os.Exit(0)
+	}()
+}
+
+/*
+Sets up the indices on the table if they don't yet exist
+- db.users.createIndex( { "email": 1 }, { unique: true } )
+*/
+func setupIndices() {
+	// Define index
+	indexModel := mongo.IndexModel{
+		Keys: bson.M{
+			"email": 1, // index in ascending order
+		},
+		Options: options.Index().SetUnique(true),
+	}
+	// Add index to user collection
+	col, _ := GetUserCollection()
+	_, err := col.Indexes().CreateOne(ctx, indexModel)
+	// Fail if error occurred
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+Load dev environment from .env if os.Getenv('GO_ENV') != 'prod'
+*/
+func loadDevEnv() {
+	// Get dev env if not prod
+	if os.Getenv("GO_ENV") != "prod" {
+		// Load .env file
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+}
