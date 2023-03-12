1dc6254d3e985c2ad82fcbf720ef71dd077f5a39
DanteLaviolette <dante.laviolette@gmail.com>
2023-03-12 19:13:16 -0400

    Create JWT cookies

---
+++ images/shared/auth/auth.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"shared/constants"
+	"shared/structs"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+/*
+Given a user & key returns a JWT auth token signed by the key as a fiber cookie.
+Returns nil if an error occurs.
+*/
+func CreateAuthCookie(user structs.User, key string) *fiber.Cookie {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"name":  user.FirstName + " " + user.LastName,
+		"iat":   time.Now().Unix(),
+		"exp":   time.Now().Unix() + constants.AuthTokenExpirySeconds,
+	})
+	signedToken, err := token.SignedString([]byte(key))
+	if err != nil {
+		return nil
+	}
+	return &fiber.Cookie{
+		Name:     "auth",
+		Value:    signedToken,
+		HTTPOnly: false,
+		SameSite: "strict",
+		MaxAge:   constants.RefreshTokenExpirySeconds,
+	}
+}
+
+/*
+Given a user & key returns a JWT refresh token signed by the key as a fiber cookie.
+Returns nil if an error occurs.
+*/
+func CreateRefreshCookie(user structs.User, key string) *fiber.Cookie {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"name":  user.FirstName + " " + user.LastName,
+		"iat":   time.Now().Unix(),
+		"exp":   time.Now().Unix() + constants.RefreshTokenExpirySeconds,
+	})
+	signedToken, err := token.SignedString([]byte(key))
+	if err != nil {
+		return nil
+	}
+	return &fiber.Cookie{
+		Name:     "refresh",
+		Value:    signedToken,
+		HTTPOnly: true,
+		SameSite: "strict",
+		MaxAge:   constants.RefreshTokenExpirySeconds,
+	}
+}
--- images/shared/constants/constants.go
+++ images/shared/constants/constants.go
@@ -3,3 +3,5 @@ package constants
 import "time"
 
 const DatabaseTimeout = 3 * time.Second
+const AuthTokenExpirySeconds = 60 * 15              // 15 mins
+const RefreshTokenExpirySeconds = 60 * 60 * 24 * 30 // 30 days
