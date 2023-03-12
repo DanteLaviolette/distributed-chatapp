7163c6478ed69bc7510416621f95202dd2a0de33
DanteLaviolette <dante.laviolette@gmail.com>
2023-03-12 12:07:07 -0400

    Insert user to db

--- images/image-b/registration/business/register.go
+++ images/image-b/registration/business/register.go
@@ -2,10 +2,16 @@ package business
 
 import (
 	"net/http"
+	"registration/persistence"
 	"registration/structs"
 
+	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
+
+const internalServerError = "Something went wrong. Try again later."
+const missingFieldsError = "Fields can't be empty"
+const duplicateKeyError = "Email already exists"
 
 /*
 Registers the user if possible based on registerInfo. Returns the response
@@ -15,15 +21,23 @@ func RegisterUser(registerInfo structs.RegisterInfo) (string, int) {
 	// Fail if fields are missing
 	if registerInfo.FirstName == "" || registerInfo.LastName == "" ||
 		registerInfo.Email == "" || registerInfo.Password == "" {
-		return "Missing fields", http.StatusBadRequest
+		return missingFieldsError, http.StatusBadRequest
 	}
 	// Hash password
 	hash, err := getPasswordHash(registerInfo.Password)
 	if err != nil {
-		return "", http.StatusInternalServerError
+		return internalServerError, http.StatusInternalServerError
 	}
+	// Set password to the hashed password
+	registerInfo.Password = hash
 	// Attempt to register user
-
+	err = persistence.InsertUser(registerInfo)
+	// Fail if error ocurred (dupe key error means email already exists)
+	if mongo.IsDuplicateKeyError(err) {
+		return duplicateKeyError, http.StatusConflict
+	} else if err != nil {
+		return internalServerError, http.StatusInternalServerError
+	}
 	return "", 200
 }
 
