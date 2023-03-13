603169fcf3100312fb6bd0ab3f3fad6a347dc2ac
DanteLaviolette <dante.laviolette@gmail.com>
2023-03-13 18:05:15 -0400

    comply w/ backend changes

--- images/image-e/frontend/src/setupProxy.js
+++ images/image-e/frontend/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
  
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api/login', { target: 'http://localhost:8000/' }))
-    app.use(createProxyMiddleware('/api/register', { target: 'http://localhost:8001/' }))
+    app.use(createProxyMiddleware('/api_login/*', { target: 'http://localhost:8000/' }))
+    app.use(createProxyMiddleware('/api_register/*', { target: 'http://localhost:8001/' }))
 }
