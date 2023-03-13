275f73c3f14a2703d185350849ef21283c9820ac
DanteLaviolette <dante.laviolette@gmail.com>
2023-03-13 18:06:47 -0400

    Display topbar

--- images/image-e/frontend/src/App.js
+++ images/image-e/frontend/src/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import TopBar from "./TopBar/TopBar";
 import { getSignedInUser } from "./utils";
 
 function App() {
   const [user, setUser] = useState(getSignedInUser())
   return (
     <div>
+      <TopBar user={user} setUser={setUser}/>
       <p>Hello World!</p>
     </div>
   );
--- images/image-e/frontend/src/TopBar/TopBar.js
+++ images/image-e/frontend/src/TopBar/TopBar.js
@@ -1,9 +1,29 @@
-function TopBar() {
-  return (
-    <div>
-      <p>Hello World!</p>
-    </div>
-  );
+import { Box, Typography } from "@mui/joy";
+import PropTypes from 'prop-types';
+import ProfileMenu from "./ProfileMenu";
+import Register from "./Register";
+import SignIn from "./SignIn";
+
+TopBar.propTypes = {
+    user: PropTypes.object,
+    setUser: PropTypes.func
+}
+
+function TopBar(props) {
+    return (
+        <Box height="50px" width="100%" sx={{
+            backgroundColor: 'background.level1',
+            padding: "0px 10px 0px 10px", display: 'inline-flex', alignItems: 'center'
+        }}>
+            <Typography level="h4">Chat App</Typography>
+            <Box flexGrow={10} flexShrink={10} />
+            {!props.user && <Box sx={{ display: 'inline-flex' }}>
+                <SignIn setUser={props.setUser} />
+                <Register setUser={props.setUser} />
+            </Box>}
+            {props.user && <ProfileMenu user={props.user} setUser={props.setUser} />}
+        </Box>
+    );
 }
 
 export default TopBar;
