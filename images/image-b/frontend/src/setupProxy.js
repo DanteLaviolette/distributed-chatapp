26c3aec7ac2cf87900131b8a1c12054810b2e0f5
DanteLaviolette <dante.laviolette@gmail.com>
2023-03-22 16:42:13 -0400

    bug fix

--- images/image-b/frontend/src/setupProxy.js
+++ images/image-b/frontend/src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use('/api/*', createProxyMiddleware({
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
     }));
     app.use(createProxyMiddleware('/ws/chat', {
-        target: 'ws://localhost:8000',
+        target: 'ws://127.0.0.1:8000',
         ws: true,
     }));
 };
